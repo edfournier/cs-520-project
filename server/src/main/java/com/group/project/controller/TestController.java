b19cab83b0117ba5b08785a35707133496f69bf9
Harsh Seth <hseth@umass.edu>
2024-12-07 19:08:36 -0500

    chore(demo): Added examples for ratings

--- server/src/main/java/com/group/project/controller/TestController.java
+++ server/src/main/java/com/group/project/controller/TestController.java
@@ -1,7 +1,6 @@
 package com.group.project.controller;  
 
 import java.util.List;
-import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,10 +10,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.group.project.entities.Course;
 import com.group.project.entities.Friendship;
+import com.group.project.entities.AggrRating;
+import com.group.project.entities.UserRating;
+import com.group.project.entities.UniClass;
 import com.group.project.entities.User;
+import com.group.project.repositories.CourseRepository;
 import com.group.project.repositories.FriendshipRepository;
+import com.group.project.repositories.AggrRatingRepository;
+import com.group.project.repositories.UserRatingRepository;
+import com.group.project.repositories.UniClassRepository;
 import com.group.project.repositories.UserRepository;
+import com.group.project.types.UniversitySession;
 
 @RestController
 @RequestMapping("/")  
@@ -25,6 +33,18 @@ public class TestController {
 
     @Autowired
     FriendshipRepository friendshipRepository;
+
+    @Autowired
+    CourseRepository courseRepository;
+
+    @Autowired
+    UniClassRepository uniClassRepository;
+
+    @Autowired
+    AggrRatingRepository aggrRatingRepository;
+
+    @Autowired
+    UserRatingRepository userRatingRepository;
 
     // Example of using private endpoint
     @GetMapping("/private/hello")  
@@ -39,16 +59,13 @@ public class TestController {
     }
 
     // testing ground for DB fns
-    @GetMapping("/test")
-    // public List<User> testFn(@RequestParam String user_1_username, @RequestParam String user_2_username) {
-    public ResponseEntity testFn(@RequestParam String user_1_username, @RequestParam String user_2_username) {
-        User user1 = new User(user_1_username, "u1", 2024, "FALL", "CS");
-        User user2 = new User(user_2_username, "u2", 2024, "FALL", "CS");
-        user1 = userRepository.save(user1);
-        user2 = userRepository.save(user2);
-        
-        user1 = userRepository.findByUsername(user2.getUsername()).orElseThrow();
-        user2 = userRepository.findByUsername(user_1_username).orElseThrow();
+    @GetMapping("/testFriendship")
+    public ResponseEntity testFriendship(@RequestParam String u1, @RequestParam String u2) {        
+        User user1 = userRepository.findByUsername(u2).orElse(new User(u2, "U2", new UniversitySession(2024, "FALL"), "Computer Science"));
+        User user2 = userRepository.findByUsername(u1).orElse(new User(u1, "U1", new UniversitySession(2024, "FALL"), "Computer Science"));
+
+        userRepository.save(user1);
+        userRepository.save(user2);
 
         // Because unidirectional graph
         if (user1.getId() > user2.getId()) {
@@ -58,7 +75,7 @@ public class TestController {
         }
 
         Friendship friendship = new Friendship(user1, user2);
-        friendshipRepository.save(friendship);
+        friendship = friendshipRepository.save(friendship); // Errors out because of SELECT on Friendship fails with "misused columns"
 
         // Stream<User> friendsStream = Stream.concat(friendshipRepository.findByUser1(user2).stream(), friendshipRepository.findByUser2(user2).stream());
 
@@ -67,6 +84,33 @@ public class TestController {
 
         // friendshipRepository.delete(friendship);
         // return friendsStream.toList();
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(friendship);
+    }
+
+    @GetMapping("/testRating")
+    public ResponseEntity<Object> testRating(@RequestParam String code, @RequestParam String semester, @RequestParam int year, @RequestParam int rating) {
+        Course course = courseRepository.findByCode(code).get();
+        UniClass uniClass = uniClassRepository.findByCourseAndSession(course, new UniversitySession(year, semester)).getFirst();
+
+        User user = userRepository.findByUsername("u1").orElseThrow();
+
+        UserRating userRating = userRatingRepository.findByUserAndUniClass(user, uniClass).orElse(new UserRating(user, uniClass, rating));
+        userRating.setRating(rating);
+        userRating = userRatingRepository.save(userRating);
+        
+        
+        return ResponseEntity.ok(userRating);
+    }
+
+    @GetMapping("/testRating2")
+    public ResponseEntity<Object> testRating2(@RequestParam String code, @RequestParam String semester, @RequestParam int year) {
+        Course course = courseRepository.findByCode(code).get();
+        
+        List<AggrRating> aggrRatings = aggrRatingRepository.findByCourse(course);
+        return ResponseEntity.ok(aggrRatings);
+
+        // UniClass uniClass = uniClassRepository.findByCourseAndSession(course, new UniversitySession(year, semester)).getFirst();
+        // AggrRating aggrRating = aggrRatingRepository.findByUniClass(uniClass).orElseThrow();        
+        // return ResponseEntity.ok(aggrRating);
     }
 }
